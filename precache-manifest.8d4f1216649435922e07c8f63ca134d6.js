3d224471e8f7ce89a727ac7de710b9c02be252e4
mavi <mavidev93@gmail.com>
2019-12-29 21:09:04 +0330

    Updates

---
+++ precache-manifest.8d4f1216649435922e07c8f63ca134d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b76dde54bf2ca17b57044d0506908cb",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "577bb63cff94d8dab2ff",
+    "url": "/monsters/static/css/main.6aaa1339.chunk.css"
+  },
+  {
+    "revision": "dfc2e96a021b35890554",
+    "url": "/monsters/static/js/2.de786c97.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.de786c97.chunk.js.LICENSE"
+  },
+  {
+    "revision": "577bb63cff94d8dab2ff",
+    "url": "/monsters/static/js/main.f4db81fd.chunk.js"
+  },
+  {
+    "revision": "34bc7ffa3e5e84c86fb8",
+    "url": "/monsters/static/js/runtime-main.90b221e2.js"
+  }
+]);
